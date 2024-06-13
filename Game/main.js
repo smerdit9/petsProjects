7ea31196c17d19f83be6e620477b21a7a2941cec
Никита Миронов <nmironov2002@gmail.com>
2024-06-14 00:59:13 +0300

    gameAdd

---
+++ Game/main.js
@@ -0,0 +1,30 @@
+// Этап 1. Создайте функцию, генерирующую массив парных чисел. Пример массива, который должна возвратить функция: [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8].count - количество пар.
+
+function createNumbersArray(count) {
+  let arr = [];
+  for (let i = 1; i <= count; ++i) {
+    arr.push(i);
+    arr.push(i);
+  }
+  return arr;
+}
+// Этап 2. Создайте функцию перемешивания массива.Функция принимает в аргументе исходный массив и возвращает перемешанный массив. arr - массив чисел
+
+function shuffle(arr) {
+  for (let i = 0; i < arr.length; ++i) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  };
+  return arr;
+}
+
+// Этап 3. Используйте две созданные функции для создания массива перемешанными номерами. На основе этого массива вы можете создать DOM-элементы карточек. У каждой карточки будет свой номер из массива произвольных чисел. Вы также можете создать для этого специальную функцию. count - количество пар.
+
+document.addEventListener('DOMContentLoaded', function () {
+  function startGame() {
+    shufflePairedNumbers = shuffle(createNumbersArray(8));
+    return shufflePairedNumbers;
+  }
+});
